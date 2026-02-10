722cb25225ac7d1e49861fc573cee585c8a1a298
Marko (Cetinić) Cettina <macetini@gmail.com>
2026-02-10 17:50:58 +0100

    refactor(view): remove unused default layout methods

--- src/core/mvcs/view/AbstractView.ts
+++ src/core/mvcs/view/AbstractView.ts
@@ -17,31 +17,6 @@ export abstract class AbstractView extends Container {
 
     public layout(w: number, h: number): void {
         console.log(`[${this.constructor.name}] Using default layout. View remains at (0,0).`);
-        const ratio = w / h;
-
-        /*if (h > w) {
-            this.onMobilePortraitLayout(w, h);
-        } else if (ratio < 1.6) {
-            // Tablet / Nest Hub Max
-            this.onTabletLayout(w, h);
-        } else {
-            // Desktop / Mobile Landscape
-            this.onDesktopLayout(w, h);
-        }*/
-    }
-
-    // Default implementations (can be overridden by children)
-    protected onTabletLayout(w: number, h: number): void {
-        console.log(`[${this.constructor.name}] Using default Tablet layout. View remains at (0,0).`);
-    }
-    protected onDesktopLayout(w: number, h: number): void {
-        console.log(`[${this.constructor.name}] Using default Desktop layout. View remains at (0,0).`);
-    }
-    protected onMobilePortraitLayout(w: number, h: number): void {
-        console.log(`[${this.constructor.name}] Using default Mobile Portrait layout. View remains at (0,0).`);
-    }
-    protected onMobileLandscapeLayout(w: number, h: number): void {
-        console.log(`[${this.constructor.name}] Using default Mobile Landscape layout. View remains at (0,0).`);
     }
 
     /**
